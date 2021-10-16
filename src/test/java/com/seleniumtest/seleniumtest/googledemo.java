5646577ceebcb1b3a04707ccca26501455272181
SrushtiB18 <87126443+SrushtiB18@users.noreply.github.com>
2021-10-17 00:00:47 +0530

    Update googledemo.java

--- src/test/java/com/seleniumtest/seleniumtest/googledemo.java
+++ src/test/java/com/seleniumtest/seleniumtest/googledemo.java
@@ -24,7 +24,7 @@ import org.testng.annotations.AfterTest;
 			public void beforeTest() {
 				System.setProperty("webdriver.chrome.whitelistedIps", "");
 				System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
-			 driver = new ChromeDriver();
+			 WebDriver driver = new ChromeDriver();
 			}
 
 			@AfterTest
